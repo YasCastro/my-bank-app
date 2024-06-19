9a366ba2c06e69f8934cbfd80beeb1a65bfd7d0d
Yasmine <yazzcastrof@gmail.com>
2024-06-19 15:47:37 -0300

    Criação de páginas e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Conta from './pages/Conta';
+import ContaInfo from './pages/ContaInfo';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Layout } from './components/Layout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ChakraProvider>
+        <Layout>
+          <Routes>
+            <Route path='/' element={< Home />} />
+            <Route path='/conta/:id' element={< Conta />} />
+            <Route path='/infoconta' element={< ContaInfo />} />
+          </Routes>
+        </Layout>
+      </ChakraProvider>
+    </BrowserRouter>
   );
 }
 
--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,13 @@
 const conta = {
-    
+    email: 'yas@mail.com',
+    password: '12345',
+    name: 'Yasmine Fortes',
+    balance: 2000.00,
+    id: '1'
 }
+
+export const api = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(conta)
+    }, 3000)
+})
