ec702700431b71da5521e0262544b59495452b90
Yasmine C. Fortes <yazzcastrof@gmail.com>
2024-07-02 17:34:31 -0300

    correção dos testes

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,18 +1,33 @@
 import { login } from "./login";
 
+const mockSetIsLoggedIn = jest.fn()
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useContext: () => ({ 
+    setIsLoggedIn: mockSetIsLoggedIn
+   })
+}))
 describe('login', () => {
   const mockAlert = jest.fn()
   window.alert = mockAlert
 
   const mockEmail = 'yas@mail.com'
 
-  it('Não deve exibir um alert de boas vindas sem o e-mail', () => {
-    login(mockEmail)
+  it('Deve exibir um alert com boas vindas caso o e-mail seja válido', async() => {
+    await login(mockEmail)
+    expect(mockSetIsLoggedIn).toHaveBeenCalledWith(true)
+    expect(mockAlert).toHaveBeenCalledWith(`Bem vinda ${mockEmail}!`)
+  })
+
+  it('Não deve exibir um alert de boas vindas sem o e-mail', async() => {
+    await login(mockEmail)
     expect(mockAlert).not.toHaveBeenCalledWith('Bem vinda!')
   })
 
-  // it('Deve exibir um erro caso o e-mail seja inválido', () => {
-  //   login('email@invalido')
-  //   expect(mockAlert).toHaveBeenCalledWith('E-mail inválido!')
-  // })
+  it('Deve exibir um erro caso o e-mail seja inválido', async() => {
+    await login('email@invalido')
+    expect(mockSetIsLoggedIn).not.toHaveBeenCalled()
+    expect(mockAlert).toHaveBeenCalledWith('E-mail inválido!')
+  })
 })
--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useContext } from "react";
 import { api } from "../api";
+import { AppContext } from "../components/AppContext";
 
 export const login = async (email: string): Promise<void> => {
+  const { setIsLoggedIn } = useContext(AppContext)
+  console.log(setIsLoggedIn)
+  
   const data: any = await api;
   if (email !== data.email)
   {
     return alert('E-mail inválido!');
   }
+  setIsLoggedIn(true)
   alert(`Bem vinda ${email}!`);
 }
