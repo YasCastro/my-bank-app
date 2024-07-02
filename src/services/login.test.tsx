d9336d3650003be5e2d846e8b2622c4be6931918
Yasmine C. Fortes <yazzcastrof@gmail.com>
2024-07-02 17:45:16 -0300

    Atualização do login

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,6 +1,7 @@
 import { login } from "./login";
 
 const mockSetIsLoggedIn = jest.fn()
+const mockNavigate = jest.fn()
 
 jest.mock('react', () => ({
   ...jest.requireActual('react'),
@@ -8,26 +9,25 @@ jest.mock('react', () => ({
     setIsLoggedIn: mockSetIsLoggedIn
    })
 }))
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom') as any,
+  useNavigate: () => mockNavigate
+}))
+
 describe('login', () => {
-  const mockAlert = jest.fn()
-  window.alert = mockAlert
 
   const mockEmail = 'yas@mail.com'
 
   it('Deve exibir um alert com boas vindas caso o e-mail seja válido', async() => {
     await login(mockEmail)
     expect(mockSetIsLoggedIn).toHaveBeenCalledWith(true)
-    expect(mockAlert).toHaveBeenCalledWith(`Bem vinda ${mockEmail}!`)
-  })
-
-  it('Não deve exibir um alert de boas vindas sem o e-mail', async() => {
-    await login(mockEmail)
-    expect(mockAlert).not.toHaveBeenCalledWith('Bem vinda!')
+    expect(mockNavigate).toHaveBeenCalledWith('/1')
   })
 
   it('Deve exibir um erro caso o e-mail seja inválido', async() => {
     await login('email@invalido')
     expect(mockSetIsLoggedIn).not.toHaveBeenCalled()
-    expect(mockAlert).toHaveBeenCalledWith('E-mail inválido!')
+    expect(mockNavigate).not.toHaveBeenCalled()
   })
 })
--- src/services/login.tsx
+++ src/services/login.tsx
@@ -2,16 +2,18 @@
 import { useContext } from "react";
 import { api } from "../api";
 import { AppContext } from "../components/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export const login = async (email: string): Promise<void> => {
   const { setIsLoggedIn } = useContext(AppContext)
   console.log(setIsLoggedIn)
-  
+  const navigate = useNavigate()
+
   const data: any = await api;
   if (email !== data.email)
   {
     return alert('E-mail inválido!');
   }
   setIsLoggedIn(true)
-  alert(`Bem vinda ${email}!`);
-}
+  navigate(`/${data.id}`) 
+ }
