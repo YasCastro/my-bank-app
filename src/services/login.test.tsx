e1ef760a9e542af255517e41b9b1f319834502d2
Yasmine <yazzcastrof@gmail.com>
2024-07-02 22:51:50 -0300

    Reorganização do service

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,33 +1,17 @@
 import { login } from "./login";
 
-const mockSetIsLoggedIn = jest.fn()
-const mockNavigate = jest.fn()
-
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useContext: () => ({ 
-    setIsLoggedIn: mockSetIsLoggedIn
-   })
-}))
-
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom') as any,
-  useNavigate: () => mockNavigate
-}))
 
 describe('login', () => {
 
   const mockEmail = 'yas@mail.com'
 
   it('Deve exibir um alert com boas vindas caso o e-mail seja válido', async() => {
-    await login(mockEmail)
-    expect(mockSetIsLoggedIn).toHaveBeenCalledWith(true)
-    expect(mockNavigate).toHaveBeenCalledWith('/1')
+    const response = await login(mockEmail)
+    expect(response).toBeTruthy()
   })
 
   it('Deve exibir um erro caso o e-mail seja inválido', async() => {
-    await login('email@invalido')
-    expect(mockSetIsLoggedIn).not.toHaveBeenCalled()
-    expect(mockNavigate).not.toHaveBeenCalled()
+    const response = await login('email@invalido')
+    expect(response).toBeFalsy()
   })
 })
--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,19 +1,14 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useContext } from "react";
 import { api } from "../api";
-import { AppContext } from "../components/AppContext";
-import { useNavigate } from "react-router-dom";
 
-export const login = async (email: string): Promise<void> => {
-  const { setIsLoggedIn } = useContext(AppContext)
-  console.log(setIsLoggedIn)
-  const navigate = useNavigate()
+
+export const login = async (email: string): Promise<boolean> => {
 
   const data: any = await api;
+
   if (email !== data.email)
   {
-    return alert('E-mail inválido!');
-  }
-  setIsLoggedIn(true)
-  navigate(`/${data.id}`) 
+    return false 
+   }
+  return true
  }
